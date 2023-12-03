4f86015b3d4a9fc2ce3fc46adb0894bbce9be5c9
ajones37 <ajones73@hawk.iit.edu>
2023-12-03 13:33:24 -0600

    change ( to {

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function init(){
 	var map = new google.maps.Map(document.getElementById('map), {
-		center: (lat: -34.397, lng: 150.664),
+		center: {lat: -34.397, lng: 150.664},
 		zoom:8
 		mapTypeId: "satellite",
 	});
